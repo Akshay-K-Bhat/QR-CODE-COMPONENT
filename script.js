2a53a474da367b93b59b0edf9b1ceae12b349f40
Akshay-K-Bhat <akshaybhat1206@gmail.com>
2024-03-09 15:16:37 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const btn = document.getElementById('submit-btn');
+const URL = document.getElementById('url');
+const qrcodeContainer = document.getElementById("qrcode")
+btn.addEventListener('click', generateQR);
+
+
+
+function generateQR(){
+
+  if(qrcodeContainer.hasChildNodes()) {
+    removeQRCode();
+  }
+
+  var qrcode = new QRCode(qrcodeContainer, {
+    text: URL.value,
+    width: 250,
+    height: 250,
+    colorDark : "#000000",
+    colorLight : "#ffffff",
+    correctLevel : QRCode.CorrectLevel.H
+  });
+
+}
+
+function removeQRCode(){
+  const element = document.getElementById("qrcode");
+  while (element.firstChild) {
+  element.removeChild(element.firstChild);
+}
+}
